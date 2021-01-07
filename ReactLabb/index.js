9f7c80613f5da7fa3c53f8877269bd6f0d661d18
NusarPayadarJava <69957044+NusarPayadarJava@users.noreply.github.com>
2021-01-07 20:02:47 +0100

    Add files via upload

---
+++ ReactLabb/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import EmployeeList from "./components/EmployeeList";
+
+
+
+ 
+
+ReactDOM.render( <EmployeeList/>,  document.getElementById('root'));
